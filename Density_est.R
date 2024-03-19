7f597fab3d94541241e03194c0bf49fc59836ca5
kpol01 <147236113+kpol01@users.noreply.github.com>
2024-03-19 15:49:08 +0530

    Update Density_est.R

--- Density_est.R
+++ Density_est.R
@@ -64,3 +64,64 @@ hist_comp_size2 <- function(n, rpdf, dens, alpha, beta)
   
   grid.arrange(grobs = plots, ncol = round(length(n)/2))
 }
+
+#function to generate obs from bimodal-normal
+rbi.norm <- function(n)
+{
+  s <- runif(n, 0, 1)
+  for(i in (1:n))
+  {
+    if(s[i] < 0.5)
+    {
+      x[i] <- qnorm(s[i] * 2 * pnorm(1), -1, 1)
+    }
+    else{
+      x[i] <- -1 * qnorm((1 - s[i]) * 2 * pnorm(1), -1, 1)
+    }
+  }
+  return(x)
+}
+
+dbi.norm <- function(x)
+{
+  f.x <- 0
+  for(i in (1:length(x)))
+  {
+    if(x[i] < 0)
+    {
+      f.x[i] <- dnorm(x[i], -1, 1) / (2 * pnorm(1))
+    }
+    else
+    {
+      f.x[i] <- dnorm(x[i], 1, 1) / (2 * pnorm(1))
+    }
+  }
+  return(f.x)
+}
+
+hist_sam_size_bimod <- function(n)
+{
+  set.seed(seed = 1)
+  s <- rbi.norm(n)
+  x <- seq(min(s), max(s), 0.001)
+  df <- data.frame(s)
+  
+  density <- dbi.norm(x)
+  df1 <- data.frame(x, density)
+  fig <- ggplot() + geom_histogram(data = df, aes(x = s, y = after_stat(density)))  + 
+    geom_line(data = df1, aes(x = x, y = density)) +
+    labs(title = paste("n = ", n))
+  return (fig)
+}
+
+hist_comp_size_bimod <- function(n)
+{
+  plots <- list()
+  for(i in (1:length(n)))
+  {
+    plots[[i]] <- hist_sam_size_bimod(n[i])
+  }
+  
+  
+  grid.arrange(grobs = plots, ncol = round(length(n)/2))
+}
